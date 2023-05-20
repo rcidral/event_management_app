2aac70b64460d4291a23c5a01f34db3fb54129da
joaovmendes19 <vitormendesj00@gmail.com>
2023-05-20 01:27:07 -0300

    develop

--- src/controllers/User.cs
+++ src/controllers/User.cs
@@ -1,17 +1,29 @@
 using System;
 using System.Collections.Generic;
 using Models;
+using System.Text.RegularExpressions;
 
     namespace Controllers{
 
         public class User{
 
-            public void store(Models.User user){
-                try{
-                    Models.User.store(user);
-                }catch(System.Exception e){
-                    throw e;
+            public void store(string nome, string email, string senha){
+                if(String.IsNullOrEmpty(nome))
+                {
+                    throw new Exception("Nome não pode ser vazio");
                 }
+                if(String.IsNullOrEmpty(email))
+                {
+                    throw new Exception("Email não pode ser vazio");
+                }
+                if(String.IsNullOrEmpty(senha))
+                {
+                    throw new Exception("Senha não pode ser vazio");
+                }
+
+                return Models.User.store(nome, email, senha);
+            }
+                
             }
 
                public List<Models.User> index(){
@@ -30,12 +42,29 @@ using Models;
                      }
                  }
 
-                 public void update(int id, Models.User user){
-                     try{
-                          Models.User.update(id, user);
-                     }catch(System.Exception e){
-                          throw e;
-                     }
+                 public void update(int id, string nome, string email, string senha){
+                        User user = Models.User.show(id);
+
+                        if(user == null)
+                        {
+                            throw new Exception("Usuário não encontrado");
+                        }
+
+                        if(String.IsNullOrEmpty(nome))
+                        {
+                            throw new Exception("Nome não pode ser vazio");
+                        }
+                        if(String.IsNullOrEmpty(email))
+                        {
+                            throw new Exception("Email não pode ser vazio");
+                        }
+                        if(String.IsNullOrEmpty(senha))
+                        {
+                            throw new Exception("Senha não pode ser vazio");
+                        }
+
+
+                     
                  }
 
                  public void delete(int id){
