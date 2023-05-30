4ef383847262e58e39a83ddb5272b6f3bc37005d
rcidral <ricidralmfo@gmail.com>
2023-05-29 21:38:11 -0300

    update menu

--- Program.cs
+++ Program.cs
@@ -1,11 +1,12 @@
+using Views;
+
 namespace Program
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            
-
+            Login.Show();
         }
     }
 }
